94138ba728eb3e6d4638a1a935cf0374397a30b2
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-03-22 10:19:48 +0600

    added types.js

---
+++ types.js
@@ -0,0 +1,9 @@
+//string type varible
+var name = "Sadar Uddin";
+var address = "Andar bandorban with some katabon"; //string type varible
+
+var country = 'Bangladesh';
+console.log(name);
+
+var isRich = true; //"" na dile hobe keyword hobe
+console.log(isRich)
