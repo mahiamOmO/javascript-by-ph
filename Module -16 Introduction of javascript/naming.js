c8ffc3c061fd7a218d67d08f9d30dc7d577fb302
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-03-22 10:36:37 +0600

    naming.js

---
+++ Module -16 Introduction of javascript/naming.js
@@ -0,0 +1,27 @@
+// 1. no keyword in varible name 
+var false = 45;
+
+// 2. no space or gap in varible name
+var is happy = false;
+var ishappy = true;
+
+
+// 3. mo quote
+var "address" = "kochu khet";
+
+//4 can not start with a number.but number other than the first letter is allowed
+var money = 45;
+var  money2 = 55;
+var 3money = 88;
+
+
+//5. name is case sensitive
+address 
+Address 
+
+
+//6.how to write a long varible 
+
+var my current home address
+
+
