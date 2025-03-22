400c107b68924d84407ed2ceaae8d2c2e8052a81
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-03-22 10:24:23 +0600

    added the types.js

--- types.js
+++ types.js
@@ -5,8 +5,15 @@ var address = "Andar bandorban with some katabon"; //string type varible
 var country = 'Bangladesh';
 console.log(name);
 
+// boolean
 var isRich = true; //"" na dile hobe keyword hobe
 console.log(isRich)
 
 var passed = false;
 console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+
+//numeric ba number
+var price = 555;
+console.log(typeof price);
