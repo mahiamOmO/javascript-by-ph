363520f626d4fd4a368479c1d9470fc8e384931b
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-03-27 13:45:54 +0600

    Added the number.js changes

--- Module -16 Introduction of javascript/number.js
+++ Module -16 Introduction of javascript/number.js
@@ -1,6 +1,10 @@
 var orangePrice = 20;
 var chocolate = 0.5;
+var applePrice = '20';
 
-console.log(typeof orangePrice);
-console.log(typeof applePrice);
+//console.log(typeof orangePrice);
+//console.log(typeof applePrice);
+
+console.log(orangePrice + applePrice);
+
 
