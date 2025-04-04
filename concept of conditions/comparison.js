0f97294534cfd6c9bcbb910ee3245ac632374bc4
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-04 14:37:25 +0600

    added comparison.js

---
+++ concept of conditions/comparison.js
@@ -0,0 +1,9 @@
+console.log( 5 > 10); // greater than
+
+console.log( 5 < 10); // less than
+
+
+console.log( 5 == 10);  //equal to
+
+
+console.log(15 >= 10); // 2 tar ekta holei true dibe kintu choto hole hbena
