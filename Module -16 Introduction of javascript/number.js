3ebcdd8a71ad716aba770702fb161b7d232b3beb
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-04 14:06:58 +0600

    updated the number.js file

--- Module -16 Introduction of javascript/number.js
+++ Module -16 Introduction of javascript/number.js
@@ -1,10 +1,25 @@
 var orangePrice = 20;
 var chocolate = 0.5;
-var applePrice = '20';
+var appleprice = '20';
+var applePrice = parseInt('22.5');
+var lichiPrice = parseFloat('22.5');
 
 //console.log(typeof orangePrice);
 //console.log(typeof applePrice);
+//console.log(typeof applePrice);
+//console.log(typeof lichiPrice);
 
-console.log(orangePrice + applePrice);
+//console.log(orangePrice + applePrice);
 
+var first = 0.1;
+var second = 0.2;
 
+var total = first + second;
+
+//console.log(total);
+
+//console.log(total.toFixed(2));
+
+const num = 50 / 0;
+
+console.log(num);
