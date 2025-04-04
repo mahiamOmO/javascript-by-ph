796f9adb0fc727d9044b159d9e37cfc8cd730fbe
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-04 14:18:39 +0600

    Added operation.js file

---
+++ Module -16 Introduction of javascript/operation.js
@@ -0,0 +1,24 @@
+var eggPrice = 10;
+var onionPrice = 20;
+
+console.log(eggPrice + onionPrice);
+
+console.log(eggPrice - onionPrice);
+console.log( onionPrice * eggPrice);
+console.log( onionPrice / eggPrice);
+
+console.log(10/2);
+console.log(10%3);
+
+var money = 500;
+
+money  = 500;
+
+var price = 35;
+
+price = price + 10;
+console.log(price);
+
+var newPrice = price - 5;
+
+console.log(newPrice);
