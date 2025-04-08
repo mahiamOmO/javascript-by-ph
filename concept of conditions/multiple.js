42c3824c98f26e3aa18ff1e9baa00e8f4558e47c
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-08 22:03:33 +0600

    added more code

--- concept of conditions/multiple.js
+++ concept of conditions/multiple.js
@@ -2,10 +2,21 @@ const salary =  250000;  //cost diye declare korle var change kora jabe na
 const isBcs = true;
 const = height = 61;
 
-if(salary > 200000 && height > 66){ //over all man ta sotto hole vitore jabena onnota te jabe
+if(salary > 200000 && height > 66){ //over all man ta sotto na hole vitore jabena onnota te jabe
     console.log('su patro');
 
 }
 else{
     console.log('onno patro khujo');
 }
+
+
+if(salary > 250000 || height > 72){
+    consolelog("eso kobul")''
+}
+else{
+    console.log('bhag tui mogbul');
+}
+
+//more and more conditions
+
