1ef5ef3932646cd970e71dad8386cbf060ffa385
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-08 22:26:13 +0600

    Added nested.js

---
+++ concept of conditions/nested.js
@@ -0,0 +1,18 @@
+const money = 500;
+if(money > 300){
+    console.log('bro your rich');
+}
+else{
+    if(money > 100){
+        console.log('tui gorib o na borolok o na');
+    }
+    else{
+        if(money > 0){
+            console.log('kola kha');
+        }
+        else {
+            console.log('tui amr bondhu na amr kache taka dhar chabi na');
+        }
+    }
+
+}
