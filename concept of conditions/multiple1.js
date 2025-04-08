db00ac1136d1a177316cc06c37a5c9e99af96569
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-08 22:09:53 +0600

    Added the multiple1.js file

---
+++ concept of conditions/multiple1.js
@@ -0,0 +1,20 @@
+const salary =  250000;  //cost diye declare korle var change kora jabe na 
+const isBcs = true;
+const = height = 61;
+
+
+if(salary > 250000 && height > 72 && isBCS == true){
+    console.log('su patro');
+}
+else {
+    console.log('onno patro khujo');
+}
+
+// ---- complex conditions
+
+if((saalry > 250000 && hasCar == true) || (isBcs == true && height > 72)){
+    console.log('eso kobul');
+}
+else {
+    console.log('bhag tui mogbul');
+}
