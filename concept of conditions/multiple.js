6789dc941e430a359d557f3177563ac898d5dbb1
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-08 21:57:54 +0600

    added multiple.js

---
+++ concept of conditions/multiple.js
@@ -0,0 +1,11 @@
+const salary =  250000;  //cost diye declare korle var change kora jabe na 
+const isBcs = true;
+const = height = 61;
+
+if(salary > 200000 && height > 66){ //over all man ta sotto hole vitore jabena onnota te jabe
+    console.log('su patro');
+
+}
+else{
+    console.log('onno patro khujo');
+}
