0ddd0d53792fdc900481486a76b08a69784ab0fb
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-09 23:22:39 +0600

    added the one value in var.js file

---
+++ array part1/one value in var.js
@@ -0,0 +1,16 @@
+//const num1 = 12;
+//const num2 = 12;
+//const num3 = 12;
+//const num4 = 12;
+
+//const numbers = 12224441123412;
+
+const numbers = [12,22,65,54];
+const friends = ['John', 'Jane', 'Jack', 'Jill'];
+const passed = [true,false,true,false];
+
+
+
+
+
+
