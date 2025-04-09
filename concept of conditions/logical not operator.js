19aa6a2303704d494f5bf8a7bba4dfeaf18f5143
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-09 23:12:13 +0600

    added logical not operator.js

---
+++ concept of conditions/logical not operator.js
@@ -0,0 +1,18 @@
+const isLeader true;
+isLeader(isLeader === true){
+    console.log('leader asche! chair charo');
+}
+else{
+    console.log('vitore dukte dibi na');
+}
+
+const isPassed = false;
+//isLeader(isPassed === false){
+if(!isPassed){
+    console.log('biye hoye jabe');
+
+}
+else{
+    console.log('tumi noyon er moni');
+
+}
