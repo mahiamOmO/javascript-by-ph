81884e68fd38bcb67999a26f937dc8e733a9e4b2
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-09 23:33:03 +0600

    added the index.js file

---
+++ fundamental concepts of array part1/index.js
@@ -0,0 +1,19 @@
+const numbers = [1,2,3,4,5,6,7];
+
+console.log(numbers);
+console.log(numbers[2]);
+
+const fourth = numbers[3];
+
+console.log(fourth);
+
+
+//set or update element value by index
+
+const abc = 45;
+
+abc = 99;
+
+numbers[1] = 99;
+
+console.log(numbers);
