d1616a059ee129f6d29745d4485ca62a5a7139f1
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-09 23:03:49 +0600

    added changes in ternery.js

--- concept of conditions/ternery.js
+++ concept of conditions/ternery.js
@@ -1,11 +1,31 @@
 /**
  * ternery - three parts
+ * 
+ *  ? : 
+ * 
+ * condition ? do something when true: do something when false 
  */
 
-const age = 20;
-if(age >=18){
-    console.log('you can vote');
+
+//const age = 20;
+//if(age >=18){
+    //console.log('you can vote');
+//}
+//else{
+    //console.log('you can not vote');
+//}
+
+
+//age >=18 ? console.log('you can vote') : console.log('you can not vote');
+
+
+let price = 500;
+const isLeader = true;
+
+if(isLeader){
+    price = 0;
 }
 else{
-    console.log('you can not vote');
+    price = price + 100;
 }
+console.log(price);
