ccecb286d29784559b6ce791d25146b8f0013471
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-14 14:23:07 +0600

    Added for.js

---
+++ loops/for.js
@@ -0,0 +1,17 @@
+let num = 0; //loop varible
+while(num < 5){ //loop condition
+    console.log(num); // loop statement
+    num++; //loop variable change
+}
+
+/** 
+ * * fort(; ; ;){
+ * *     //loop statement}
+ */
+
+for(first part;second part; third part){
+    //loop statement
+}
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
