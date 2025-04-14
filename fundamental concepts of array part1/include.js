bc83a3c965938a59ec5ce2ec525ba874b1e56c3d
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-14 11:08:22 +0600

    added the file includes.js

---
+++ fundamental concepts of array part1/include.js
@@ -0,0 +1,11 @@
+const friends = ['balam', 'kalam', 'salam', 'dalam', 'malam'];
+
+console.log(friends.includes.apply('gelam'))
+console.log(friends.includes('khailam'))
+    console.log('party')
+if(friends.includes('salam')){
+    console.log('party');
+}
+else{
+    console.log('no food.we are not going to party');
+}
