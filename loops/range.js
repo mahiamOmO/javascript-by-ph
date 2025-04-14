4b1c3b735174bb6fcbf823afa8d8dc1c466cb54b
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-14 13:03:31 +0600

    added range.js changes

--- loops/range.js
+++ loops/range.js
@@ -1,5 +1,11 @@
-let num = 1;
-while(num < 10){
+//let num = 1;
+//while(num <= 10){ // focus on the conditions
+   // console.log(num);
+   // num++;
+//}
+
+let num = 50;
+while(num <= 100){
     console.log(num);
     num++;
 }
