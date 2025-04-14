1bf769fc9a3024731cc5e68d041192b6ab3cbe3b
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-04-14 11:20:18 +0600

    Added while.js

---
+++ loops/while.js
@@ -0,0 +1,12 @@
+const number = 7;
+if(number < 1){
+    console.log('choto akta number paisi');
+}
+console.log('something new is cooking');
+
+
+// -----while--loop-----
+const num = 0;
+while(num<5){
+    console.log('looping');
+}
